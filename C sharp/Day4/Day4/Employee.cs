a223cada9ad6261fb38944e7e946618e0e0b5534
Jahnavi Ogety <jahnavio@infinite.com>
2024-06-17 22:57:43 +0530

    Monday

---
+++ C sharp/Day4/Day4/Employee.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day4
+{
+    class Employee
+    {
+        int Empid;
+        string EmpName;
+        DateTime DOJ;
+        double salary;
+
+        //1.constructor
+
+        public Employee()
+        {
+            Empid = 10;
+            EmpName = "Infinite";
+            DOJ = Convert.ToDateTime("2/2/24");
+            salary = 50000;
+
+        }
+
+        //  2.constructor
+         public Employee(int eid,string name,float sal)
+         {
+            Empid = eid;
+           EmpName = name;
+            salary = sal;
+
+         }
+        public void GetEmployeeDetails()
+        {
+            Console.WriteLine("Enter id,name,sal and DOJ :");
+            Empid = Convert.ToInt32(Console.ReadLine());
+            EmpName = Console.ReadLine();
+            salary = Convert.ToSingle(Console.ReadLine());
+            DOJ = Convert.ToDateTime(Console.ReadLine());
+        }
+        public void ShowEmpDetails()
+        {
+            Console.WriteLine($"Employee id{Empid}, Employee name {EmpName}, Employee salary{salary}, date of joining {DOJ} ");
+            Console.Read();
+        }
+    }
+    class Emptest
+    {
+        public static void Main()
+        {
+            Employee empp = new Employee();
+            empp.ShowEmpDetails();
+            Console.WriteLine("-------------");
+            Employee emp2 = new Employee(22,"janu", 3800);
+            
+           emp2.ShowEmpDetails();
+
+
+        }
+    }
+
+}
---
+++ C sharp/Day4/Day4/Enumerations.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day4
+{
+    enum cities { Banglore,Chennai,Delhi,Hyderabad,Agra}
+    class Enumerations
+    {
+        enum days { Monday,Tuesday,Wednesday}
+
+       public static void EnumOp()
+        {
+            foreach(int x in Enum.GetValues(typeof(cities)))
+            {
+                if(x==1)
+                
+                    Console.WriteLine(Enum.GetName(typeof(cities),x) + " it is garden city");
+                else if(x==2)
+                    Console.WriteLine(Enum.GetName(typeof(cities), x) +  " it is temple city");
+
+
+
+
+            }
+            foreach(var c in Enum.GetNames(typeof(cities)))
+            {
+                Console.WriteLine(c);
+            }
+        }
+
+
+    }
+}
