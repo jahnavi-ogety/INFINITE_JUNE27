aa33a8221fbe6dde0bf58eadc3bdc81cd77065c4
Jahnavi Ogety <jahnavio@infinite.com>
2024-07-03 13:07:16 +0530

    Assesment3

---
+++ C sharp/Assesments/CC3/CC3/Program1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CC3
+{
+    public class Cricket
+    {
+        public int[] scores;
+
+
+
+        public void Pointscalculation(int no_of_matches)
+        {
+            scores = new int[no_of_matches];
+
+
+            for (int i = 0; i < no_of_matches; i++)
+            {
+                Console.Write($"Enter the score for match {i + 1}: ");
+                scores[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+
+            int sum = 0;
+            foreach (int score in scores)
+            {
+                sum += score;
+            }
+            double average = (double)sum / no_of_matches;
+            Console.WriteLine("---------------------------");
+            Console.WriteLine($"sum of the match is: {sum}");
+            Console.WriteLine($"average of the match  is: {average}");
+        }
+    }
+
+    class Program_1
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter number of matches: ");
+            int matches_played = Convert.ToInt32(Console.ReadLine());
+
+            Cricket cricket = new Cricket();
+            cricket.Pointscalculation(matches_played);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ C sharp/Assesments/CC3/CC3/Program2_Test.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CC3
+{
+    public class Box
+    {
+        public double Length { get; set; }
+        public double Breadth { get; set; }
+
+        public Box(double l, double b)
+        {
+            Length = l;
+            Breadth = b;
+        }
+        public static Box Add(Box box1, Box box2)
+        {
+            double n1 = box1.Length + box2.Length;
+            double n2 = box1.Breadth + box2.Breadth;
+            return new Box(n1, n2);
+        }
+
+        public void Display()
+        {
+            Console.WriteLine($"Length and Breadth of the Box - Length: {Length}, Breadth: {Breadth}");
+        }
+    }
+
+    public class Program2_Test
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Length and Breath of  Box 1:");
+            Console.Write("Length: ");
+            double length1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Breadth: ");
+            double breadth1 = Convert.ToDouble(Console.ReadLine());
+
+
+            Box box1 = new Box(length1, breadth1);
+
+            Console.WriteLine("Length and Breath of Box 2:");
+            Console.Write("Length: ");
+            double length2 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Breadth: ");
+            double breadth2 = Convert.ToDouble(Console.ReadLine());
+
+            Box box2 = new Box(length2, breadth2);
+            Box box3 = Box.Add(box1, box2);
+            Console.WriteLine("Details of Box 3:");
+            box3.Display();
+            Console.Read();
+        }
+    }
+}
+
