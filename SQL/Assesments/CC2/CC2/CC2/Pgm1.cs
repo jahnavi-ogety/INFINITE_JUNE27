d30fa6f0f9eb3656374101b29ac082beca2d9ef3
Jahnavi Ogety <jahnavio@infinite.com>
2024-07-24 11:35:35 +0530

    Assesment

---
+++ SQL/Assesments/CC2/CC2/CC2/Pgm1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+ 
+namespace CC2
+{
+    class Pgm1
+    {
+        public static SqlConnection con;
+        public static SqlCommand cmd;
+        
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter employee name");
+            string name = Console.ReadLine();
+            Console.WriteLine("enter employee salary");
+            decimal sal = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("enter employeeType");
+            char emptype = Convert.ToChar(Console.ReadLine());
+
+            con = new SqlConnection("data source=ICS-LT-CT2RQ73\\SQLEXPRESS; initial catalog=employeeManagement;" +
+                "user id=sa;password=123456789;");
+            con.Open();
+            cmd = new SqlCommand("InsertEmp_Details", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@empname", name);
+            cmd.Parameters.AddWithValue("empsal", sal);
+            cmd.Parameters.AddWithValue("@emptype", emptype);
+
+            int Total_rows = cmd.ExecuteNonQuery();
+            Console.WriteLine($"Inserted the rows effected successfully.");
+            Console.Read();
+        }
+    }
+}
